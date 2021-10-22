6f2dd8dba8e1653408cb4a6fc34282657507b482
Jin Park <jinpark1504@gmail.com>
2021-10-21 21:22:03 -0400

    create add form

--- js/default.js
+++ js/default.js
@@ -1,4 +1,12 @@
-const complete = document.querySelector('.todo-complete');
-complete.addEventListener('click', () => {
-  complete.classList.toggle('completed');
-})
+const complete = document.querySelectorAll('.todo-complete, .todo-details');
+complete.forEach(evt => {
+  evt.addEventListener('click', () => {
+    console.log(evt);
+    document.querySelector('.todo-complete').classList.toggle('completed');
+  })
+});
+const add_todo = document.querySelector('#todo-add');
+const add_form = document.querySelector('#add-form');
+add_todo.addEventListener('click', () => {
+  add_form.classList.add('active');
+});
--- js/nav.js
+++ js/nav.js
@@ -1,5 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const sidebar = document.getElementsByClassName('sidebar-trigger')[0];
+  const sidebar = document.querySelector('.sidebar-trigger');
+  const side_menu = document.querySelector('#side-menu');
+  const btn_add = document.querySelector('#todo-add');
+  const form_add = document.querySelector('#add-form');
+  const submit_add = document.querySelector('#add-form button[type="submit"]');
   const overlay = document.querySelector('#overlay');
   sidebar.addEventListener('click', () => {
     sidebar.childNodes.forEach(child => {
@@ -7,15 +11,29 @@ document.addEventListener('DOMContentLoaded', () => {
         child.classList.toggle('close');
       }
     });
-    document.querySelector('#side-menu').classList.toggle('show');
-    overlay.classList.toggle('show');
+    // CHECK IF ADD FORM IS ACTIVE
+    if(form_add.classList.contains('active')) {
+      form_add.classList.remove('active');
+    } else {
+      overlay.classList.toggle('show');
+    }
+    side_menu.classList.toggle('show');
     hide_overlay();
   });
+  btn_add.addEventListener('click', () => {
+    form_add.classList.add('active');
+    overlay.classList.add('show');
+    hide_overlay();
+  });
+  submit_add.addEventListener('click', () => {
+    form_add.classList.remove('active');
+    overlay.classList.remove('show');
+  });
   const hide_overlay = () => {
-    console.log(document.querySelector('#overlay.show'));
     if(document.querySelector('#overlay.show')) {
       document.querySelector('#overlay.show').addEventListener('click', () => {
-        document.querySelector('#side-menu').classList.remove('show');
+        side_menu.classList.remove('show');
+        form_add.classList.remove('active');
         overlay.classList.remove('show');
       })
     }
